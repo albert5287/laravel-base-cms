6d96b3d41056d17946eb21c0db60ae7d7fcad6d5
unknown <albert.gracia@innova-new-ED800.ecrome.com>
2015-09-30 10:46:23 +0200

    -change layout for the forms

--- resources/views/language/partials/form.blade.php
+++ resources/views/language/partials/form.blade.php
@@ -1,9 +1,5 @@
 <div class="box box-primary">
     <div class="box-body">
-        <div class="form-group">
-            {!! Form::label('code', trans('strings.HEADER_TABLE_FOR_CODE_IN_LANGUAGES').':') !!}
-            {!! Form::text('code', null, ['class' => 'form-control']) !!}
-        </div>
         <div class="nav-tabs-custom">
             <ul class="nav nav-tabs">
                 @foreach(getActiveLanguages() as $index => $lang)
@@ -13,6 +9,10 @@
                 @endforeach
             </ul>
             <div class="tab-content">
+                <div class="form-group">
+                    {!! Form::label('code', trans('strings.HEADER_TABLE_FOR_CODE_IN_LANGUAGES').':') !!}
+                    {!! Form::text('code', null, ['class' => 'form-control']) !!}
+                </div>
                 @foreach(getActiveLanguages() as $index => $lang)
                     <div class="tab-pane {{$index == 0 ? 'active' : ''}}" id="tab_{{$lang->code}}">
                         <div class="form-group">
@@ -21,10 +21,13 @@
                         </div>
                     </div><!-- /.tab-pane -->
                 @endforeach
-            </div><!-- /.tab-content -->
-        </div><!-- nav-tabs-custom -->
+            </div>
+            <!-- /.tab-content -->
+        </div>
+        <!-- nav-tabs-custom -->
 
-    </div><!-- /.box-body -->
+    </div>
+    <!-- /.box-body -->
     <div class="box-footer">
 
         {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
